84e85910ce06ba3cfe8b88a2c40bbc4b27978ea2
Lahcim1988 <michalurbanek@op.pl>
2020-03-21 14:15:21 +0100

    brackets

---
+++ src/codility/brackets/Solution.java
@@ -0,0 +1,43 @@
+package codility.brackets;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println(checkIfBracketsHavePairs("[()]{}{[()()]()}"));
+        System.out.println(checkIfBracketsHavePairs("()[]}"));
+
+    }
+
+    private static boolean checkIfBracketsHavePairs(String br) {
+
+        String [] arr = br.split("");
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < br.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(arr[i]);
+            } else {
+                if (arr[i].equals("[") && stack.lastElement().equals("]")) {
+                    stack.pop();
+                }else if (arr[i].equals("]") && stack.lastElement().equals("[")) {
+                    stack.pop();
+                }else if (arr[i].equals("(") && stack.lastElement().equals(")")) {
+                    stack.pop();
+                }else if (arr[i].equals(")") && stack.lastElement().equals("(")) {
+                    stack.pop();
+                }else if (arr[i].equals("{") && stack.lastElement().equals("}")) {
+                    stack.pop();
+                }else if (arr[i].equals("}") && stack.lastElement().equals("{")) {
+                    stack.pop();
+                }else {
+                    stack.push(arr[i]);
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
